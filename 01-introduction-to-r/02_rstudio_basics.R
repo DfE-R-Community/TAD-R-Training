8a08dd87d22384979cea20a8820bbe8cde3eae6c
Wojciech Klimowicz <wojciech.klimowicz@education.gov.uk>
2024-01-19 15:26:07 +0000

    Initial website

---
+++ 01-introduction-to-r/02_rstudio_basics.R
@@ -0,0 +1,50 @@
+# RStudio -----------------------------
+# * NOT R. RStudio is an Integrated Development Environment (IDE) for coding.
+# * You can write R code using notepad if you want to.
+# * But RStudio is recommended because it makes life easier
+
+# RStudio Layout ------------------------
+
+# Top left is your script/source code. All your analysis goes here.
+# Your source code is your "work" -- the objects you've made are temporary,
+# and you should feel comfortable deleting them.
+
+# Top right is environment (+ others). It lets you see current objects.
+
+# Bottom left is console - you can write and send code there.
+
+# Bottom right is file explorer, but also plot window
+# Ctrl + Enter to run code
+rnorm(n = 100, mean = 0, sd = 1) |>
+  hist()
+
+# Ctrl 1 and Ctrl 2 to swap between console and editor.
+
+# RStudio Projects --------------------
+# * Keep files together
+# * Sort out working directory
+
+# More Keyboard Shortcuts ---------------------
+# use them!
+# Ctrl + Enter - runs the block of code - use this after every line of code
+5 + 5
+
+# Ctrl + Shift + Enter - run the entire file
+
+# Ctrl + Tab
+# Ctrl + Shift + Tab
+# Ctrl + S, Ctrl + Z etc...
+
+# Tab-Complete.
+# Use this as often as Ctrl + Enter
+
+mean()
+
+# ? - help
+?mean
+
+# Most importantly - Google Google Google
+# 90% of the time, it works every time
+# If you can't find what you need, or don't know what it's called, ask me.
+
+# Recently: Bing Enterprise Chat, it's very good.
